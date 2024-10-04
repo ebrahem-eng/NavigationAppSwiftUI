ca3e3b23bfa6246192ecfcdca70b21b7228364f6
Ebrahem Alwish <ebrahemk968@gmail.com>
2024-10-04 22:02:41 +0300

    Initial Commit

---
+++ NavigationApp/NavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppApp.swift
+//  NavigationApp
+//
+//  Created by Ebrahem-Alwish on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
